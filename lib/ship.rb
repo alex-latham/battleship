90b7c7c9bf0a6c0910247093c751aee15e590f88
Alex Latham <alexlthm@gmail.com>
2020-02-04 11:57:52 -0700

    Add name method and test

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,4 +1,5 @@
 class Ship
+  attr_reader :name
 
   def initialize(name_parameter, length_parameter)
     @name = name_parameter
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -8,4 +8,10 @@ class ShipTest < Minitest::Test
 
     assert_instance_of Ship, cruiser
   end
+
+  def test_it_has_name_and_length
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal "Cruiser", cruiser.name
+  end
 end
