5aedefcb767ee89d3d907cc6513d34e747f5d29e
Alex Latham <alexlthm@gmail.com>
2020-02-04 12:28:24 -0700

    Refactor & modify tests

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -9,20 +9,32 @@ class ShipTest < Minitest::Test
     assert_instance_of Ship, cruiser
   end
 
-  def test_it_has_name_and_length
+  def test_it_has_a_name
     cruiser = Ship.new("Cruiser", 3)
 
     assert_equal "Cruiser", cruiser.name
+  end
+
+  # Combine with previous test?
+  def test_it_has_a_length
+    cruiser = Ship.new("Cruiser", 3)
+
     assert_equal 3, cruiser.length
   end
+
 
   def test_it_tracks_health
     cruiser = Ship.new("Cruiser", 3)
 
     assert_equal 3, cruiser.health
+
+    cruiser.hit
+
+    assert_equal 2, cruiser.health
+
   end
 
-  def test_if_can_sink
+  def test_it_can_sink
     cruiser = Ship.new("Cruiser", 3)
 
     assert_equal false, cruiser.sunk?
@@ -32,12 +44,5 @@ class ShipTest < Minitest::Test
     cruiser.hit
 
     assert_equal true, cruiser.sunk?
-  end
-
-  def test_it_can_take_hit
-    cruiser = Ship.new("Cruiser", 3)
-    cruiser.hit
-
-    assert_equal 2, cruiser.health
   end
 end
