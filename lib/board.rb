ea1010eaebe4b441fe75d870565c18800adcdde0
Alex Latham <alexlthm@gmail.com>
2020-02-04 17:39:29 -0700

    Add test to check that cells method return appropriate hash

--- lib/board.rb
+++ lib/board.rb
@@ -2,4 +2,25 @@ class Board
 
   def initialize
   end
+
+  def cells
+    hash = {}
+    hash["A1"] = Cell.new("A1")
+    hash["A2"] = Cell.new("A2")
+    hash["A3"] = Cell.new("A3")
+    hash["A4"] = Cell.new("A4")
+    hash["B1"] = Cell.new("B1")
+    hash["B2"] = Cell.new("B2")
+    hash["B3"] = Cell.new("B3")
+    hash["B4"] = Cell.new("B4")
+    hash["C1"] = Cell.new("C1")
+    hash["C2"] = Cell.new("C2")
+    hash["C3"] = Cell.new("C3")
+    hash["C4"] = Cell.new("C4")
+    hash["D1"] = Cell.new("D1")
+    hash["D2"] = Cell.new("D2")
+    hash["D3"] = Cell.new("D3")
+    hash["D4"] = Cell.new("D4")
+    hash
+  end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -7,7 +7,15 @@ require './lib/board'
 class BoardTest < Minitest::Test
   def test_it_can_exist
     board = Board.new
-
     assert_instance_of Board, board
+  end
+
+  def test_it_has_cells
+    board = Board.new
+    hash = board.cells
+
+    assert_equal Hash, hash.class
+    assert_equal 16, hash.length
+    assert_instance_of Cell, hash["A1"]
   end
 end
