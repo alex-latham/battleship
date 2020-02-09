cb3d6d6eaf08bc4daa17bb56655dca458f486eec
Steven Meyers <sjjmeyers@gmail.com>
2020-02-09 10:05:46 -0700

    Rename methods and add helper methods to setup

--- lib/game.rb
+++ lib/game.rb
@@ -9,43 +9,43 @@ class Game
     @player_submarine = ships_parameter[3]
   end
 
-  def start
+  def main_menu
     puts "Welcome to BATTLESHIP"
     puts "Enter p to play. Enter q to quit."
 
     response = gets.chomp.downcase
 
     return abort("Fine, byeeeeeee.") if response == "q"
-    return play_game if response == "p"
-    return start
+    return setup if response == "p"
+    return main_menu
   end
 
-  def play_game
+  def setup
     place_computer_ship(@computer_cruiser)
     place_computer_ship(@computer_submarine)
 
     puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
     puts "The Cruiser is three units long and the Submarine is two units long."
+
     puts @player_board.render(true)
+    place_player_ship(@player_cruiser)
+    place_player_ship(@player_submarine)
 
-    puts "Enter the squares for the Cruiser (3 spaces):"
+    turn
+  end
+
+  def turn
+    display_boards
+  end
+
+  def place_player_ship(ship_parameter)
+    puts "Enter the squares for the #{ship_parameter.name} (#{ship_parameter.length} spaces):"
     response = gets.chomp.upcase.split(" ")
-
-    until @player_board.place(@player_cruiser, response) != nil do
+    until @player_board.place(ship_parameter, response) != nil do
       puts "Those are invalid coordinates. Please try again:"
       response = gets.chomp.upcase.split(" ")
     end
-    puts @player_board.render(true)
-
-    puts "Enter the squares for the Submarine (2 spaces):"
-    response = gets.chomp.upcase.split(" ")
-
-    until @player_board.place(@player_submarine, response) != nil do
-      puts "Those are invalid coordinates. Please try again:"
-      response = gets.chomp.upcase.split(" ")
-    end
-
     puts @player_board.render(true)
   end
 
@@ -56,5 +56,18 @@ class Game
         ship_coordinates = @computer_board.place(ship_parameter, random_coordinates)
       end
   end
+# remove true argument for computer board when finished debugging
+  def display_boards
+    puts "=============COMPUTER BOARD============="
+    puts @computer_board.render(true)
+    puts "=============PLAYER BOARD==============="
+    puts @player_board.render(true)
+  end
 
+  def player_shot
+    puts "Enter the coordinate for your shot:"
+    response = gets.chomp.upcase
+    
+
+  end
 end
