9faa4324973dc49f602953b076664a62ec0681a4
Alex Latham <alexlthm@gmail.com>
2020-02-09 15:09:34 -0700

    Update game test file to match changes to Game class

--- test/game_test.rb
+++ test/game_test.rb
@@ -8,28 +8,34 @@ require './lib/game'
 class GameTest < Minitest::Test
 
   def setup
-    @boards = [Board.new, Board.new]
-    @ships = [Ship.new("Cruiser", 3),
-      Ship.new("Submarine", 2),
-      Ship.new("Cruiser", 3),
-      Ship.new("Submarine", 2)]
+    @computer_data = {
+      "computer_board" => Board.new,
+      "cruiser" => Ship.new("Cruiser", 3),
+      "submarine" => Ship.new("Submarine", 2)
+    }
+
+    @player_data = {
+      "player_board" => Board.new,
+      "cruiser" => Ship.new("Cruiser", 3),
+      "submarine" => Ship.new("Submarine", 2)
+    }
+
+    @game = Game.new(@computer_data, @player_data)
   end
 
   def test_it_can_exist
-    game = Game.new(@boards, @ships)
 
-    assert_instance_of Game, game
+    assert_instance_of Game, @game
   end
 
   def test_computer_can_choose_valid_placement
-    game = Game.new(@boards, @ships)
-    game.place_computer_ship(@ships[0])
-    occupied_cells = @boards[0].cells.values.count{ |cell| cell.ship != nil}
+    @game.place_computer_ship(@computer_data["cruiser"])
+    occupied_cells = @computer_data["computer_board"].cells.values.count{ |cell| cell.ship != nil}
 
     assert_equal 3, occupied_cells
 
-    game.place_computer_ship(@ships[1])
-    occupied_cells = @boards[0].cells.values.count{ |cell| cell.ship != nil}
+    @game.place_computer_ship(@computer_data["submarine"])
+    occupied_cells = @computer_data["computer_board"].cells.values.count{ |cell| cell.ship != nil}
     assert_equal 5, occupied_cells
   end
 
