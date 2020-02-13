5d589db2f02d3770c5861bee7645502e8aa18189
Alex Latham <alexlthm@gmail.com>
2020-02-12 22:26:01 -0700

    Added setups to tests, removed redundant assertions, reordered to match pry interation patterns exactly.

--- test/board_test.rb
+++ test/board_test.rb
@@ -5,125 +5,100 @@ require './lib/cell'
 require './lib/board'
 
 class BoardTest < Minitest::Test
+
+  def setup
+    @board = Board.new
+    @board.generate_cells(4)
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
   def test_it_can_exist
-    board = Board.new
-    assert_instance_of Board, board
+    assert_instance_of Board, @board
   end
 
   def test_it_has_cells
-    board = Board.new
-    hash = board.cells
-
-    assert_equal Hash, hash.class
-    assert_equal 16, hash.length
-    assert_instance_of Cell, hash["A1"]
+    assert_equal Hash, @board.cells.class
+    assert_equal 16, @board.cells.length
+    assert_instance_of Cell, @board.cells["C3"]
   end
 
   def test_if_coordinate_is_valid
-    board = Board.new
-
-    assert_equal true, board.valid_coordinate?("A1")
-    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal true, @board.valid_coordinate?("A1")
+    assert_equal true, @board.valid_coordinate?("D4")
+    assert_equal false, @board.valid_coordinate?("A5")
+    assert_equal false, @board.valid_coordinate?("E1")
+    assert_equal false, @board.valid_coordinate?("A22")
   end
 
-  def test_if_coordinates_are_valid
-    board = Board.new
-
-    assert_equal true, board.valid_coordinates?(["A1", "A2", "A3"])
-    assert_equal false, board.valid_coordinates?(["A1", "A2", "A5"])
+  def test_it_can_check_coordinates_length_against_ship_length
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
   end
 
-  def test_if_target_coordinates_are_empty
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal true, board.target_coordinates_empty?(["A1", "A2", "A3"])
-
-    board.place(cruiser, ["A1", "A2", "A3"])
-
-    assert_equal false, board.target_coordinates_empty?(["A1", "A2", "A3"])
+  def it_can_check_coordinates_are_consecutive
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
   end
 
-  def test_it_can_validate_placement
-    board = Board.new
-    # board.generate_cells(4)
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    # cruiser
-    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
-    assert_equal true, board.valid_placement?(cruiser, ["A2", "A1", "A3"])
-
-    # submarine
-    assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
-    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+  def it_can_check_for_diagonal_coordinates
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C2", "D3"])
   end
 
-  def test_if_placement_coordinates_are_consecutive
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    # cruiser
-    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    assert_equal true, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-
-    # submarine
-    assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
-    assert_equal true, board.valid_placement?(submarine, ["C1", "B1"])
-  end
-
-  def test_if_placement_coordinates_are_diagonals
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    assert_equal false, board.valid_placement?(cruiser, ["A1", "B2", "C3"])
-    assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
+  def it_can_validate_acceptable_coordinates
+    assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
 
   def test_if_can_place_ship
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    cell_1 = board.cells["A1"]
-    cell_2 = board.cells["A2"]
-    cell_3 = board.cells["A3"]
-    board.place(cruiser, ["A1", "A2", "A3"])
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    cell_1 = @board.cells["A1"]
+    cell_2 = @board.cells["A2"]
+    cell_3 = @board.cells["A3"]
 
-    assert_equal cruiser, cell_1.ship
-    assert_equal cruiser, cell_2.ship
-    assert_equal cruiser, cell_3.ship
+    assert_equal @cruiser, cell_1.ship
+    assert_equal @cruiser, cell_2.ship
+    assert_equal @cruiser, cell_3.ship
     assert_equal cell_3.ship, cell_2.ship
   end
 
   def test_ships_cannot_overlap
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    board.place(cruiser, ["A1", "A2", "A3"])
-    submarine = Ship.new("Submarine", 2)
+    @board.place(@cruiser, ["A1", "A2", "A3"])
 
-    assert_equal false, board.valid_placement?(submarine, ["A1", "B1"])
-  end
-
-  def test_if_valid_target
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    board.place(cruiser, ["A1", "A2", "A3"])
-
-    assert_equal true, board.valid_target?("A1")
-
-    board.cells["A1"].fire_upon
-    assert_equal false, board.valid_target?("A1")
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "B1"])
   end
 
   def test_board_can_render
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    board.place(cruiser, ["A1", "A2", "A3"])
-    blank_board = "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
-    ship_board = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    render_false = "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
+    render_true = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
 
-    assert_equal blank_board, board.render
-    assert_equal ship_board, board.render(true)
+    assert_equal render_false, @board.render
+    assert_equal render_true, @board.render(true)
   end
+
+  def test_it_cannot_fire_on_same_coordinate_twice
+    assert_equal true, @board.valid_target?("A1")
+
+    @board.cells["A1"].fire_upon
+
+    assert_equal false, @board.valid_target?("A1")
+  end
+
+  def test_if_coordinates_are_valid
+    assert_equal true, @board.valid_coordinates?(["A1", "A2", "A3"])
+    assert_equal false, @board.valid_coordinates?(["A1", "A2", "A5"])
+  end
+
+  def test_if_target_coordinates_are_empty
+    assert_equal true, @board.target_coordinates_empty?(["A1", "A2", "A3"])
+
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+
+    assert_equal false, @board.target_coordinates_empty?(["A1", "A2", "A3"])
+  end
+
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -3,39 +3,39 @@ require 'minitest/pride'
 require './lib/ship'
 
 class ShipTest < Minitest::Test
-  def test_it_can_exist
-    cruiser = Ship.new("Cruiser", 3)
 
-    assert_instance_of Ship, cruiser
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_can_exist
+    assert_instance_of Ship, @cruiser
   end
 
   def test_it_has_attributes
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal "Cruiser", cruiser.name
-    assert_equal 3, cruiser.length
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+    assert_equal 3, @cruiser.health
   end
 
   def test_it_tracks_health
-    cruiser = Ship.new("Cruiser", 3)
+    @cruiser.hit
 
-    assert_equal 3, cruiser.health
-
-    cruiser.hit
-
-    assert_equal 2, cruiser.health
-
+    assert_equal 2, @cruiser.health
   end
 
   def test_it_can_sink
-    cruiser = Ship.new("Cruiser", 3)
+    assert_equal false, @cruiser.sunk?
 
-    assert_equal false, cruiser.sunk?
+    @cruiser.hit
+    @cruiser.hit
 
-    cruiser.hit
-    cruiser.hit
-    cruiser.hit
+    assert_equal 1, @cruiser.health
+    assert_equal false, @cruiser.sunk?
 
-    assert_equal true, cruiser.sunk?
+    @cruiser.hit
+
+    assert_equal true, @cruiser.sunk?
   end
+
 end
